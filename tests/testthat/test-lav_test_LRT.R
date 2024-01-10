3d0fe9dc4c9fd4165fa4b7aa5a66a0ba4926c23f
laywang142 <122498550+laywang142@users.noreply.github.com>
2024-01-10 10:21:40 -0800

    more tests

--- tests/testthat/test-lav_test_LRT.R
+++ tests/testthat/test-lav_test_LRT.R
@@ -12,6 +12,18 @@ testthat::test_that("Returns dataframe when no errors present", {
   expect_s3_class(res, "data.frame")
 })
 
+testthat::test_that("Returns dataframe when no errors present - single argument", {
+  HS.model <- '
+    visual  =~ x1 + b1*x2 + x3
+    textual =~ x4 + b2*x5 + x6
+    speed   =~ x7 + b3*x8 + x9
+'
+  fit1 <- cfa(HS.model, data = HolzingerSwineford1939)
+  res <- lavTestLRT(fit1)
+  
+  expect_s3_class(res, "data.frame")
+})
+
 testthat::test_that("Returns error message when method + test combo invalid", {
   HS.model <- '
     visual  =~ x1 + b1*x2 + x3
@@ -24,7 +36,8 @@ testthat::test_that("Returns error message when method + test combo invalid", {
   expect_error(lavTestLRT(fit1, fit0,
                           type = "browne",
                           method = "delta"),
-               "method cannot be used if type is browne.residual.adf or browne.residual.nt")
+               label = "method cannot be used if type is browne.residual.adf or
+               browne.residual.nt")
 })
 
 testthat::test_that("Returns error message when method invalid", {
@@ -38,7 +51,36 @@ testthat::test_that("Returns error message when method invalid", {
               orthogonal = TRUE)
   expect_error(lavTestLRT(fit1, fit0,
                           method = "invalid"),
-               "unknown method for scaled difference test")
+               label = "unknown method for scaled difference test")
 })
 
+testthat::test_that("Returns warning message when type invalid - single argument", {
+  
+  HS.model <- '
+    visual  =~ x1 + b1*x2 + x3
+    textual =~ x4 + b2*x5 + x6
+    speed   =~ x7 + b3*x8 + x9
+'
+  fit1 <- cfa(HS.model, data = HolzingerSwineford1939)
+  expect_warning(lavTestLRT(fit1, type = "cf"), 
+               label = "`type' argument is ignored for a single model")
+})
 
+testthat::test_that("Returns warning message when variables in models different", {
+  
+  HS.model <- '
+    visual  =~ x1 + b1*x2 + x3
+    textual =~ x4 + b2*x5 + x6
+    speed   =~ x7 + b3*x8 + x9
+'
+  
+  HS.model1 <- '
+    visual  =~ x1 + b1*x2 + x3
+    textual =~ x4 + b2*x5 + x6
+'
+  fit1 <- cfa(HS.model, data = HolzingerSwineford1939)
+  fit0 <- cfa(HS.model1, data = HolzingerSwineford1939, 
+              orthogonal = TRUE)
+  expect_warning(lavTestLRT(fit1, type = "cf"), 
+               label = "some models are based on a different set of observed variables")
+})
