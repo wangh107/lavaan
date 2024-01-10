0362632459b8301c0d049dae43fa6e46df2dada7
laywang142 <122498550+laywang142@users.noreply.github.com>
2024-01-10 11:50:05 -0800

    more tests

--- tests/testthat/test-lav_test_LRT.R
+++ tests/testthat/test-lav_test_LRT.R
@@ -84,3 +84,51 @@ testthat::test_that("Returns warning message when variables in models different"
   expect_warning(lavTestLRT(fit1, type = "cf"), 
                label = "some models are based on a different set of observed variables")
 })
+
+testthat::test_that("Returns warning when not all models have meanstructure", {
+  
+  HS.model <- '
+    visual  =~ x1 + b1*x2 + x3
+    textual =~ x4 + b2*x5 + x6
+    speed   =~ x7 + b3*x8 + x9
+'
+  
+  fit1 <- cfa(HS.model, data = HolzingerSwineford1939,
+              meanstructure = TRUE)
+  fit0 <- cfa(HS.model, data = HolzingerSwineford1939, 
+              orthogonal = TRUE)
+  expect_warning(expect_error(lavTestLRT(fit1, fit0),
+                              label = "lavaan ERROR: some models (but not all) have scaled test statistics"), 
+                              label = "not all models have a meanstructure")
+  
+})
+
+testthat::test_that("Returns warning when not all models have converged", {
+    data = head(HolzingerSwineford1939, 6)
+    model_1 <- 'f =~ x1 + x2 + x3 + x4 + x5'
+    suppressWarnings({
+      fit1 <- cfa(model_1, data = data)
+    })
+    
+    expect_warning(
+    expect_warning(lavTestLRT(fit1),
+                 label = "model did not converge"),
+    label = "model did not converge")
+})
+
+testthat::test_that("Returns warning message for no robust test statistics", {
+  HS.model <- '
+    visual  =~ x1 + b1*x2 + x3
+    textual =~ x4 + b2*x5 + x6
+    speed   =~ x7 + b3*x8 + x9
+'
+  fit1 <- cfa(HS.model, data = HolzingerSwineford1939)
+  fit0 <- cfa(HS.model, data = HolzingerSwineford1939, 
+              orthogonal = TRUE)
+  expect_warning(lavTestLRT(fit1, fit0, type = "chisq",
+             test = "scaled.shifted",
+             method = "satorrabentler2010"),
+             label = paste("lavaan WARNING: method = satorrabentler2010",
+                           "\n\t but no robust test statistics were used;",
+                           "\n\t switching to the standard chi-square difference test"))
+})
