216e45430a3b36be6d544865bbf48299b770cc1d
Youshi Zhang <zhang.youshi@gene.com>
2024-01-30 19:12:56 +0000

    Basic functionality

---
+++ tests/testthat/test-parameterEstimates.R
@@ -0,0 +1,41 @@
+# Basic functionality
+## Note: integrate with fit functions
+library(lavaan)
+data = HolzingerSwineford1939
+model_1<- 'f  =~ x1 + x2 + x3 + x4 + x5 + x6+  x7 + x8 + x9 '
+fit_1 <- cfa(model_1, data = data)
+parameterEstimates(fit_1)
+
+test_that("parameterEstimates return the correct class", {
+  HS.model <- 'visual  =~ x1 + x2 + x3 
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+  fit <- cfa(HS.model, data=HolzingerSwineford1939)
+  if (inherits(fit, "lavaan")){ # Sanity check: the input needs to be lavaan class
+    res <- parameterEstimates(fit)
+    expect_s3_class(res, "lavaan.data.frame")
+    expect_s3_class(res, "data.frame")
+  }
+})
+
+test_that("parameterEstimates returns dataframe with correct columns", {
+  HS.model <- 'visual  =~ x1 + x2 + x3 
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+  fit <- cfa(HS.model, data=HolzingerSwineford1939)
+  if (inherits(fit, "lavaan")){ # Sanity check: the input needs to be lavaan class
+    res <- parameterEstimates(fit)
+    expected_columns <- c("lhs", "op", "rhs", "est", "se", "z", "pvalue", "ci.lower", "ci.upper")
+    expect_named(res, expected_columns)
+  }
+})
+
+test_that("parameterEstimates and ParameterEstimates are simply alias", {
+  HS.model <- 'visual  =~ x1 + x2 + x3 
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+  fit <- cfa(HS.model, data=HolzingerSwineford1939)
+  res_1 <- parameterEstimates(fit)
+  res_2 <- parameterestimates(fit)
+  expect_identical(res_1, res_2)
+})
