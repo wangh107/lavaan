cd55262d86d7ed39d0cd759c22ae948fec91cff7
Laura Wang <wang.laura@gene.com>
2024-03-26 18:06:00 +0000

    check if wrapper

--- tests/testthat/test-summary.R
+++ tests/testthat/test-summary.R
@@ -27,3 +27,37 @@ testthat::test_that("summary returns correct class - efa", {
   expect_true(is.list(res))
 
 })
+
+testthat::test_that("Behaves correctly with default arguments", {
+
+  HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+  fit <- cfa(HS.model, data = HolzingerSwineford1939)
+
+  res <- summary(fit)
+
+  expect_true(inherits(res, "lavaan.summary"))
+  expect_true(is.list(res))
+
+})
+
+testthat::test_that("summary is indeed a wrapper for lav_object_summary", {
+
+  HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+  fit <- cfa(HS.model, data = HolzingerSwineford1939)
+
+  mock_lav_object_summary <- function(object, ...) {
+    expect_s4_class(object, "lavaan")
+    return(TRUE) # Simulate successful execution
+  }
+  local_mocked_bindings(
+    `lav_object_summary` = mock_lav_object_summary
+  )
+  expect_true(summary(fit))
+
+})
