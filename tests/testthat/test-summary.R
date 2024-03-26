7c1dfc922dfc4a7c14636c600be510502ed8e122
Laura Wang <wang.laura@gene.com>
2024-03-26 17:19:02 +0000

    basic tests

---
+++ tests/testthat/test-summary.R
@@ -0,0 +1,29 @@
+testthat::test_that("summary returns correct class - cfa", {
+
+  HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+  fit <- cfa(HS.model, data = HolzingerSwineford1939)
+
+  res <- summary(fit, standardized = TRUE, fit.measures = TRUE, rsquare = TRUE)
+
+  expect_true(inherits(res, "lavaan.summary"))
+  expect_true(is.list(res))
+
+})
+
+testthat::test_that("summary returns correct class - efa", {
+
+  fit <- efa(data = HolzingerSwineford1939,
+             ov.names = paste("x", 1:9, sep = ""),
+             nfactors = 1:3,
+             rotation = "geomin",
+             rotation.args = list(geomin.epsilon = 0.01, rstarts = 1))
+  res <- summary(fit, nd = 3L, cutoff = 0.2, dot.cutoff = 0.05)
+
+  expect_true(inherits(res, "efaList.summary"))
+  expect_true(all(c("cutoff", "dot.cutoff") %in% names(attributes(res))))
+  expect_true(is.list(res))
+
+})
