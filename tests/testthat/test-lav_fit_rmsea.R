b4a65d6041d74ad0ff975b3e6d0107cda42cb55d
Laura Wang <wang.laura@gene.com>
2024-04-02 21:15:40 +0000

    add tests

--- tests/testthat/test-lav_fit_rmsea.R
+++ tests/testthat/test-lav_fit_rmsea.R
@@ -1,1 +1,25 @@
+testthat::test_that("Returns a number - standard method", {
 
+  res <- lav_fit_rmsea(X2 = 518.178, df = 267, N = 1328)
+
+  expect_true(is.numeric(res))
+
+})
+
+
+testthat::test_that("Returns a number - population version", {
+
+  res <- lav_fit_rmsea(F.val = 100, df = 267)
+
+  expect_true(is.numeric(res))
+
+})
+
+
+testthat::test_that("Returns NA when nothing supplied", {
+
+  res <- lav_fit_rmsea()
+
+  expect_true(is.na(res))
+
+})
