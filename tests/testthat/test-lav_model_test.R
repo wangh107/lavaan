a58967ccff2f85c139e3fc211cf461d5d1305626
Laura Wang <wang.laura@gene.com>
2024-04-04 20:49:25 +0000

    more tests

--- tests/testthat/test-lav_model_test.R
+++ tests/testthat/test-lav_model_test.R
@@ -15,7 +15,7 @@ testthat::test_that("Returns sacaled output when provided scaled test", {
               textual =~ x4 + x5 + x6
               speed   =~ x7 + x8 + x9 '
 
-  fit <- cfa(HS.model, data = data, test = c("mean.var.adjusted", "scaled.shifted"))
+  fit <- cfa(HS.model, data = HolzingerSwineford1939, test = c("mean.var.adjusted", "scaled.shifted"))
 
   res <- lav_model_test(fit)
 
@@ -45,7 +45,21 @@ testthat::test_that("Returns empty list - test = none", {
   expect_equal(res[[1]], expected[[1]])
 })
 
-## PML
+
+testthat::test_that("p-val = 0 when df = 0", {
+
+  HS.model <- ' x1 ~ 1'
+
+  fit <- cfa(HS.model, data = HolzingerSwineford1939, test = "scaled.shifted")
+
+  res <- lav_model_test(fit)
+
+  expect_equal(res[[1]]$pvalue, as.numeric(NA))
+  expect_equal(res[[2]]$pvalue, as.numeric(NA))
+})
+
+
+# ## PML
 # testthat::test_that("Returns output when estimator = PML", {
 #
 #   HS9 <- HolzingerSwineford1939[,c("x1","x2","x3","x4","x5",
@@ -62,10 +76,74 @@ testthat::test_that("Returns empty list - test = none", {
 #   expect_true(is.list(res))
 #
 # })
+#
+# ## Warning for test case not available for PML
+#
+# testthat::test_that("Returns output when estimator = PML", {
+#
+#   HS9 <- HolzingerSwineford1939[,c("x1","x2","x3","x4","x5",
+#                                    "x6","x7","x8","x9")]
+#   HSbinary <- as.data.frame( lapply(HS9, cut, 2, labels=FALSE) )
+#
+#   # Single group example with one latent factor
+#   HS.model <- ' trait =~ x1 + x2 + x3 + x4 '
+#   fit <- cfa(HS.model, data=HSbinary[,1:4], ordered=names(HSbinary[,1:4]),
+#              estimator="PML", test = "scaled.shifted")
+#
+#   expect_warning(lav_model_test(fit),
+#                   "test option scaled.shifted not available for estimator PML")
+#
+# })
 
-## two-level
+## Various test types
 
-## Warning for test case not available for PML
+testthat::test_that("Returns correct class - browne", {
+  HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+  fit <- cfa(HS.model, data = HolzingerSwineford1939,
+             test = c("browne.residual.adf",
+                      "browne.residual.adf.model",
+                      "browne.residual.nt",
+                      "browne.residual.nt.model"))
+
+  res <- lav_model_test(fit)
+
+  expect_true(is.list(res))
+})
+
+testthat::test_that("Returns correct class - yuan.bentler", {
+  HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+  fit <- suppressWarnings(cfa(HS.model, data = HolzingerSwineford1939,
+             test = c("yuan.bentler",
+                      "yuan.bentler.mplus")))
+
+  res <- lav_model_test(fit)
+
+  expect_true(is.list(res))
+})
+
+testthat::test_that("Returns correct class - bollen.stine", {
+
+  HS.model <- ' visual  =~ x1 + x2 + x3
+              textual =~ x4 + x5 + x6
+              speed   =~ x7 + x8 + x9 '
+
+  fit <- suppressWarnings(cfa(HS.model, data = HolzingerSwineford1939,
+                              test = c("bollen.stine"),
+                              bootstrap = 10))
+
+  res <- lav_model_test(fit)
+
+  expect_true(is.list(res))
+
+})
+
+
 
 
 
