216bdb5281607172375c14a049c8442200c10f4f
Youshi Zhang <zhang.youshi@gene.com>
2024-04-23 22:38:02 +0000

    Change objects to all caps

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,19 +1,21 @@
+# NOTE: all objects below are in ALL CAPS to differentiate with other objects
+
 # Fit a CFA using HolzingerSwineford1939
-model_cfa_HS <- "visual  =~ x1 + x2 + x3
+MODEL_CFA_HS <- "visual  =~ x1 + x2 + x3
                 textual =~ x4 + x5 + x6
                 speed   =~ x7 + x8 + x9"
-fit_cfa_HS <- cfa(model_cfa_HS, data = HolzingerSwineford1939)
+FIT_CFA_HS <- cfa(MODEL_CFA_HS, data = HolzingerSwineford1939)
 
 # Fit a EFA using HolzingerSwineford1939
-model_efa_HS <- '
+MODEL_EFA_HS <- '
     efa("efa")*f1 + 
     efa("efa")*f2 + 
     efa("efa")*f3 =~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
 '
-fit_efa_HS <- cfa(model_efa_HS, data = HolzingerSwineford1939)
+FIT_EFA_HS <- cfa(MODEL_EFA_HS, data = HolzingerSwineford1939)
 
 # Fit a SEM using Political Democracy 1989
-model_sem_PD <- '
+MODEL_SEM_PD <- '
   # measurement model
     ind60 =~ x1 + x2 + x3
     dem60 =~ y1 + y2 + y3 + y4
@@ -28,10 +30,10 @@ model_sem_PD <- '
     y4 ~~ y8
     y6 ~~ y8
 '
-fit_sem_PD <- sem(model_sem_PD, data = PoliticalDemocracy)
+FIT_SEM_PD <- sem(MODEL_SEM_PD, data = PoliticalDemocracy)
 
 # Fit Demo.twolevels
-model_sem_twolevels <- '
+MODEL_SEM_TWOLEVELS <- '
     level: 1
         fw =~ y1 + y2 + y3
         fw ~ x1 + x2 + x3
@@ -39,4 +41,4 @@ model_sem_twolevels <- '
         fb =~ y1 + y2 + y3
         fb ~ w1 + w2
 '
-fit_sem_twolevels <- sem(model = model_sem_twolevels, data = Demo.twolevel, cluster = "cluster")
+FIT_SEM_TWOLEVELS <- sem(model = MODEL_SEM_TWOLEVELS, data = Demo.twolevel, cluster = "cluster")
