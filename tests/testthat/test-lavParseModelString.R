19d93107ad8c281c154ac325701731ede47686f0
Youshi Zhang <zhang.youshi@gene.com>
2024-05-09 20:46:30 +0000

    focus on wrapper part

--- tests/testthat/test-lavParseModelString.R
+++ tests/testthat/test-lavParseModelString.R
@@ -8,34 +8,17 @@ test_that("lavParseModelString's parser argument only takes certain string", {
   expect_equal(res_cap, res_nocap)
 })
 
-test_that("lavParseModelString takes input example from documentation", {
-  full_model <- '# 1. latent variable definitions
-                f1 =~ y1 + y2 + y3
-                f2 =~ y4 + y5 + y6
-                f3 =~ y7 + y8 +
-                y9 + y10
-                f4 =~ y11 + y12 + y13
-                ! this is also a comment
-                # 2. regressions
-                f1 ~ f3 + f4
-                f2 ~ f4
-                y1 + y2 ~ x1 + x2 + x3
-                # 3. (co)variances
-                y1 ~~ y1
-                y2 ~~ y4 + y5
-                f1 ~~ f2
-                # 4. intercepts
-                f1 ~ 1; y5 ~ 1
-                # 5. thresholds
-                y11 | t1 + t2 + t3
-                y12 | t1
-                y13 | t1 + t2
-                # 6. scaling factors
-                y11 ~*~ y11
-                y12 ~*~ y12
-                y13 ~*~ y13
-                # 7. formative factors
-                f5 <~ z1 + z2 + z3 + z4
-                '
-  expect_silent(lavParseModelString(full_model))
+# wrapper
+test_that("calling lavParseModelString is same as calling underlying function", {
+  # Current default value is new parser
+  res1 <- lavParseModelString(MODEL_CFA_HS, 
+                              as.data.frame. = FALSE,
+                              parser = "new",
+                              warn = TRUE, debug = FALSE)
+  res2 <- ldw_parse_model_string(model.syntax = MODEL_CFA_HS,
+                                 as.data.frame. = FALSE,
+                                 warn = TRUE, debug = FALSE)
+  expect_equal(res1, res2)
+  
+  # TODO: old parser
 })
