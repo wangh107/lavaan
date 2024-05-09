207845e6d08b19648211512be9d40c8fd32f159b
Youshi Zhang <zhang.youshi@gene.com>
2024-05-09 19:56:29 +0000

    Add extra argument

--- tests/testthat/test-lav_model_get_parameters.R
+++ tests/testthat/test-lav_model_get_parameters.R
@@ -29,6 +29,25 @@ test_that("lav_model_get_parameters's type only takes specified string", {
   expect_error(lav_model_get_parameters(lavmodel = lav_model, type = "invalid"))
 })
 
+## Extra
+test_that("lav_model_get_parameters correctly handle extra", {
+  # example: https://lavaan.ugent.be/tutorial/syntax2.html
+  set.seed(1234)
+  Data <- data.frame(y  = rnorm(100), 
+                     x1 = rnorm(100), 
+                     x2 = rnorm(100),
+                     x3 = rnorm(100))
+  model.constr <- ' # model with labeled parameters
+                    y ~ b1*x1 + b2*x2 + b3*x3
+                  # constraints
+                    b1 == (b2 + b3)^2
+                    b1 > exp(b2 + b3) '
+  fit <- sem(model.constr, data = Data)
+  expect_silent(lav_model_get_parameters(fit@Model, type = "user", extra = TRUE))
+  
+  #TODO: fit@Model@x.def.idx
+})
+
 # Output
 test_that("lav_model_get_parameters return a vector of double", {
   est <- lav_model_get_parameters(FIT_CFA_HS@Model)
