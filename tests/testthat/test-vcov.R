1f2e065a27e327b810f6ffdef0f6b84d12a38c1d
Youshi Zhang <zhang.youshi@gene.com>
2024-06-07 23:54:18 +0000

    add tests for user defined test

--- tests/testthat/test-vcov.R
+++ tests/testthat/test-vcov.R
@@ -25,12 +25,28 @@ test_that("vcov handles type argument correctly", {
     vcov(FIT_CFA_HS, type = "novalid"),
     "type argument should be \"user\" or \"free\""
   )
-  expect_silent(vcov(FIT_CFA_HS, type = "user"))
   expect_silent(vcov(FIT_CFA_HS, type = "free"))
-  expect_silent(vcov(FIT_CFA_HS, type = "joint"))
-  expect_silent(vcov(FIT_CFA_HS, type = "all"))
-  expect_silent(vcov(FIT_CFA_HS, type = "full"))
-  expect_silent(vcov(FIT_CFA_HS, type = "complete"))
+  # user defined parmeter
+  X <- rnorm(100)
+  M <- 0.5*X + rnorm(100)
+  Y <- 0.7*M + rnorm(100)
+  Data <- data.frame(X = X, Y = Y, M = M)
+  model <- ' # direct effect
+             Y ~ c*X
+           # mediator
+             M ~ a*X
+             Y ~ b*M
+           # indirect effect (a*b)
+             ab := a*b
+           # total effect
+             total := c + (a*b)
+         '
+  fit <- sem(model, data = Data)
+  expect_silent(vcov(fit, type = "user"))
+  expect_silent(vcov(fit, type = "joint"))
+  expect_silent(vcov(fit, type = "all"))
+  expect_silent(vcov(fit, type = "full"))
+  expect_silent(vcov(fit, type = "complete"))
 })
 
 test_that("vcov throws error for user + remove.duplicated combo", {
