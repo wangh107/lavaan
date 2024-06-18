36d7443f8dd89c547d28e92f807c30ea346569b1
Laura Wang <wang.laura@gene.com>
2024-06-18 17:08:25 +0000

    make more specific

--- tests/testthat/test-print.lavaan.matrix.symmetric.R
+++ tests/testthat/test-print.lavaan.matrix.symmetric.R
@@ -7,15 +7,24 @@ test_that("returns output if passed a matrix", {
 test_that("adds '.' to replace NA on diagonal", {
   y <- matrix(c(NA,2,3,4,NA,6,7,8,NA), ncol = 3, nrow = 3)
 
-  expect_output(print.lavaan.matrix.symmetric(y, diag.na.dot = TRUE))
+  expect_output(print.lavaan.matrix.symmetric(y, diag.na.dot = TRUE),
+                "\\.")
 })
 
 test_that("prints column and row names", {
   y <- matrix(c(1,2,3,4,5,6,7,8,9), ncol = 3, nrow = 3)
+
+  expect_output(print.lavaan.matrix.symmetric(y, shift = 1L),
+                "\\n\\s")
+
   rownames(y) <- c("A", "B", "C")
   colnames(y) <- c("D", "E", "F")
 
-  expect_output(print.lavaan.matrix.symmetric(y, shift = 1L))
+  expect_output(print.lavaan.matrix.symmetric(y, shift = 1L),
+                "\\n\\sA")
+  expect_output(print.lavaan.matrix.symmetric(y),
+                "\\nA")
+
 })
 
 test_that("prints header and footer attributes", {
@@ -23,5 +32,8 @@ test_that("prints header and footer attributes", {
   attr(y, "header") <- "header"
   attr(y, "footer") <- "footer"
 
-  expect_output(print.lavaan.matrix.symmetric(y, shift = 1L))
+  expect_output(print.lavaan.matrix.symmetric(y),
+                "header")
+  expect_output(print.lavaan.matrix.symmetric(y),
+                "footer")
 })
